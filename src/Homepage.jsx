771c2ea937b422af962a98ee3120a3a7001ec000
shraaavss <shravs2707@gmail.com>
2023-12-03 11:57:26 +0530

    changed img, cleaned algnmt

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Spotify from './Spotify';
 import cup from './/assets/coffeeeee.png';
 import './General.css';
@@ -8,48 +8,55 @@ import AmbientSoundPlayer from './AudioPlayer';
 import rain from "./assets/rain-sound.mp3";
 import beanPour from './assets/coffee-bean-pour-into-pan.mp3';
 import dripCof from './assets/drip-coffee-maker.mp3';
-import people from './assets/people-talking2.mp3'
-import pouring from './assets/pouring-coffee.mp3'
-
+import people from './assets/people-talking2.mp3';
+import pouring from './assets/pouring-coffee.mp3';
+import cofpour from './assets/cofeepour.png';
 
 export default function Homepage() {
-    return (
-        <>
-            <Header />
+  const [isPlaying, setIsPlaying] = useState(false);
 
-            <div className="mainwrap w-screen h-screen flex p-8">
+  const handlePauseAll = () => {
+    setIsPlaying(false);
+  };
 
-                <div className="subwrap flex flex-row flex-wrap 
-            justify-around justify-items-center content-center items-center">
-                    <div className="block1 basis-1/3">
-                        <h1 className="text-6xl font-title text-title-brown mb-2">Your Cafe</h1>
-                        <h3 className="subtitle text-xl font-body text-title-brown">Hi there, how may I help you?</h3>
-                        <Spotify />
-                    </div>
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
 
-                    <div className="block2 basis-1/3">
-                        <img src={cup} alt="" />
-                    </div>
+      <div className="mainwrap flex flex-grow p-10 items-center">
 
-                    <div className="block3 basis-1/3">
-                        <AmbientSoundPlayer soundPath={rain} />
-                        <br />
-                        <AmbientSoundPlayer soundPath={beanPour} />
-                        <br />
-                        <AmbientSoundPlayer soundPath={dripCof} />
-                        <br />
-                        <AmbientSoundPlayer soundPath={people} />
-                        <br />
-                        <AmbientSoundPlayer soundPath={pouring} />
-                        <br />
-                        <br />
-                    </div>
+        <div className="subwrap flex flex-row w-full justify-around items-center justify-items-center">
 
-                </div>
+          <div className="block1 flex-1">
+            <h1 className="text-6xl font-title text-title-brown mb-2">Your Cafe</h1>
+            <h3 className="subtitle text-xl font-body text-title-brown">Hi there, how may I help you?</h3>
+            <Spotify />
+          </div>
 
-            </div>
-        </>
+          <div className="block2 flex-1 text-center">
+            <img src={cofpour} alt="" className="mx-auto max-w-full h-auto" />
+          </div>
 
-
-    )
+          <div className="block3 flex-1">
+            {isPlaying && (
+              <button onClick={handlePauseAll} className="mb-4">
+                Pause All Sounds
+              </button>
+            )}
+            <AmbientSoundPlayer soundPath={rain} label="Rainy Day" setIsPlaying={setIsPlaying} />
+            <br />
+            <AmbientSoundPlayer soundPath={beanPour} label="Bean Pour" setIsPlaying={setIsPlaying} />
+            <br />
+            <AmbientSoundPlayer soundPath={dripCof} label="Drip Coffee" setIsPlaying={setIsPlaying} />
+            <br />
+            <AmbientSoundPlayer soundPath={people} label="People Talking" setIsPlaying={setIsPlaying} />
+            <br />
+            <AmbientSoundPlayer soundPath={pouring} label="Pouring" setIsPlaying={setIsPlaying} />
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
